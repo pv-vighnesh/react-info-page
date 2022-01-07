bb69ba858474043466b7cc8157d16b4ee71d5d80
pv-vighnesh <pvighnesh.vishnu@gmail.com>
2022-01-08 00:56:01 +0530

    Adding navbar and main page component

---
+++ src/components/Main.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Main() {
+    return (
+        <div>
+            <h1>Main content</h1>
+            <p>This is main content</p>
+        </div>
+    )
+}
+
+export default Main;
